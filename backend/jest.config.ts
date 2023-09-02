6d575e559c102ea3c3766e8c833b2525353bf9cb
username <790768275@qq.com>
2023-09-02 20:16:53 +0800

    test: 配置jest

---
+++ backend/jest.config.ts
@@ -0,0 +1,31 @@
+import type { Config } from '@jest/types'
+
+const config: Config.InitialOptions = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  coverageDirectory: 'coverage',
+  collectCoverage: true,
+  testPathIgnorePatterns: ['/node_modules/'],
+  transform: {
+    '^.+\\.ts?$': 'ts-jest'
+  },
+  testMatch: ['<rootDir>/src/**/test/*.ts'],
+  collectCoverageFrom: ['src/**/*.ts', '!src/**/test/*.ts?(x)', '!**/node_modules/**'],
+  coverageThreshold: {
+    global: {
+      branches: 1,
+      functions: 1,
+      lines: 1,
+      statements: 1
+    }
+  },
+  coverageReporters: ['text-summary', 'lcov'],
+  moduleNameMapper: {
+    '@/(.*)': ['<rootDir>/src/$1'],
+    '@feature/(.*)': ['<rootDir>/src/feature/$1'],
+    '@shared/(.*)': ['<rootDir>/src/shared/$1']
+  }
+}
+
+export default config
