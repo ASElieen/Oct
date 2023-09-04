e125742c4517221baf2b8aeec3c1b25161a0057f
username <790768275@qq.com>
2023-09-04 10:02:38 +0800

    feat: 完善get post, 实现从mongodb中get数据

--- backend/src/shared/services/db/post.service.ts
+++ backend/src/shared/services/db/post.service.ts
@@ -1,6 +1,6 @@
 import { UpdateQuery } from 'mongoose'
 
-import { IPostDocument } from '@feature/posts/interfaces/post.interface'
+import { IPostDocument, IGetPostsQuery } from '@feature/posts/interfaces/post.interface'
 import { PostModel } from '@/feature/posts/models/post.schema'
 import { IUserDocument } from '@feature/user/interfaces/user.interface'
 import { UserModel } from '@/feature/user/models/user.schemal'
@@ -11,6 +11,30 @@ class PostService {
     //increase
     const user: UpdateQuery<IUserDocument> = UserModel.updateOne({ _id: userId }, { $inc: { postsCount: 1 } })
     await Promise.all([post, user])
+  }
+
+  public async getPosts(query: IGetPostsQuery, skip = 0, limit = 0, sort: Record<string, 1 | -1>): Promise<IPostDocument[]> {
+    let postQuery = {}
+    if (query.imgId && query?.gifUrl) {
+      postQuery = { $or: [{ imgId: { $ne: '' } }, { gifUrl: { $ne: '' } }] }
+    } else {
+      postQuery = query
+    }
+
+    const posts = await PostModel.aggregate([
+      { $match: postQuery },
+      { $sort: sort },
+      //pagnation
+      { $skip: skip },
+      { $limit: limit }
+    ])
+    return posts
+  }
+
+  public async postsCount(): Promise<number> {
+    //find({})表示计算总数
+    const count = await PostModel.find({}).countDocuments()
+    return count
   }
 }
 
