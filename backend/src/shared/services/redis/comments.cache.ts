661728631d2ed7af295d1b0c87bb90550cfedcb5
username <790768275@qq.com>
2023-09-09 10:39:02 +0800

    feat: 补充redis的get，添加获取所有评论用户和获取单个评论方法

--- backend/src/shared/services/redis/comments.cache.ts
+++ backend/src/shared/services/redis/comments.cache.ts
@@ -1,10 +1,12 @@
 import Logger from 'bunyan'
+import { find } from 'lodash'
 
 import { BaseCache } from './base.cache'
 import { config } from '@/config'
 import { ServerError } from '@/shared/global/helpers/errorHandler'
 import { Helpers } from '@/shared/global/helpers/helper'
 import { ICommentDocument } from '@feature/comments/interfaces/comments.interface'
+import { ICommentNameList } from '@feature/comments/interfaces/comments.interface'
 
 const logger: Logger = config.createLogger('commentsCache')
 
@@ -47,6 +49,53 @@ export class CommentsCache extends BaseCache {
     } catch (error) {
       logger.error(error)
       throw new ServerError('从redis中取出评论内容时失败,请重试')
+    }
+  }
+
+  public async getCommentsNameFromCache(postId: string): Promise<ICommentNameList> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect()
+      }
+      const commentsCount = await this.client.LLEN(`comments:${postId}`)
+      const commentsInCache = await this.client.LRANGE(`comments:${postId}`, 0, -1)
+
+      const list: string[] = []
+      for (const item of commentsInCache) {
+        const comment: ICommentDocument = Helpers.parseJSON(item)
+        list.push(comment.username)
+      }
+
+      const resp: ICommentNameList = {
+        count: commentsCount,
+        names: list
+      }
+      return resp
+    } catch (error) {
+      logger.error(error)
+      throw new ServerError('从redis中取出评论用户时失败,请重试')
+    }
+  }
+
+  public async getSingleCommentFromCache(postId: string, commentId: string): Promise<ICommentDocument> {
+    try {
+      if (!this.client.isOpen) {
+        await this.client.connect()
+      }
+
+      const commentsInCache = await this.client.LRANGE(`comments:${postId}`, 0, -1)
+      const list: ICommentDocument[] = []
+      for (const comment of commentsInCache) {
+        list.push(Helpers.parseJSON(comment))
+      }
+
+      const singleComment = find(list, (listItem: ICommentDocument) => {
+        return listItem._id === commentId
+      }) as ICommentDocument
+      return singleComment
+    } catch (error) {
+      logger.error(error)
+      throw new ServerError('从redis中取出单个评论时失败,请重试')
     }
   }
 }
