a5bb85560ef996b4e023051ea988a2bd05bd289c
username <790768275@qq.com>
2023-09-20 16:56:07 +0800

    feat:（some_users_feature）补充socketIO事件

--- backend/src/shared/sockets/chat.ts
+++ backend/src/shared/sockets/chat.ts
@@ -1,6 +1,8 @@
 import { ISenderReceiver } from '@/feature/chat/interfaces/chat.interface'
 import { Server, Socket } from 'socket.io'
 
+import { connectedUserMap } from './user'
+
 export let socketIOChatObject: Server
 
 export class SocketIOChatHandler {
@@ -14,7 +16,13 @@ export class SocketIOChatHandler {
   public listen(): void {
     this.io.on('connection', (socket: Socket) => {
       socket.on('join room', (data: ISenderReceiver) => {
-        console.log(data)
+        const { senderName, receiverName } = data
+        //通过username拿到socket.id
+        const senderSocketId: string = connectedUserMap.get(senderName) as string
+        const receiverSocketId: string = connectedUserMap.get(receiverName) as string
+        //join room
+        socket.join(senderSocketId)
+        socket.join(receiverSocketId)
       })
     })
   }
--- backend/src/shared/sockets/user.ts
+++ backend/src/shared/sockets/user.ts
@@ -1,7 +1,10 @@
 import { ISocketData } from '@/feature/user/interfaces/user.interface'
 import { Server, Socket } from 'socket.io'
+import { ILogin } from '../../feature/user/interfaces/user.interface'
 
 export let socketIOUserObject: Server
+export const connectedUserMap: Map<string, string> = new Map()
+let users: string[] = []
 
 export class SocketIOUserHandler {
   private io: Server
@@ -12,6 +15,18 @@ export class SocketIOUserHandler {
 
   public listen(): void {
     this.io.on('connection', (socket: Socket) => {
+      socket.on('setup', (data: ILogin) => {
+        //socket.id发送至client
+        this.addClientToMap(data.userId, socket.id)
+        this.addUser(data.userId)
+        this.io.emit('user online', users)
+      })
+
+      socket.on('disconnected', () => {
+        //client返回socket.id
+        this.removeClientFromMap(socket.id)
+      })
+
       socket.on('block user', (data: ISocketData) => {
         this.io.emit('block user id', data)
       })
@@ -20,5 +35,31 @@ export class SocketIOUserHandler {
         this.io.emit('unblock user id', data)
       })
     })
+  }
+
+  private addClientToMap(username: string, socketId: string): void {
+    if (!connectedUserMap.has(username)) {
+      connectedUserMap.set(username, socketId)
+    }
+  }
+
+  private removeClientFromMap(socketId: string): void {
+    if (Array.from(connectedUserMap.values()).includes(socketId)) {
+      const disconnectedUser: [string, string] = [...connectedUserMap].find((user: [string, string]) => {
+        return user[1] === socketId
+      }) as [string, string]
+      connectedUserMap.delete(disconnectedUser[0])
+      this.removeUser(disconnectedUser[0])
+      this.io.emit('user online', users)
+    }
+  }
+
+  private addUser(username: string): void {
+    users.push(username)
+    users = [...new Set(users)]
+  }
+
+  private removeUser(username: string): void {
+    users = users.filter((name: string) => name != username)
   }
 }
